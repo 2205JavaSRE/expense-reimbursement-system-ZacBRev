f6f7a04867c10eeb649d03492e9dfeca6325e3f1
ZacBurnett <zachary710@revature.net>
2022-05-31 12:17:07 -0700

    Initial Commit Creation of Project POJO - Employee and ReimbursementTicket Creation of Packages Models, Dao, Service, Controller

--- src/main/java/com/revature/models/Employee.java
+++ src/main/java/com/revature/models/Employee.java
@@ -1,5 +1,90 @@
 package com.revature.models;
 
+import java.util.List;
+
 public class Employee {
     private int employeeID;
+    private String username;
+    private String password;
+    private String position;
+    private String firstName;
+    private String lastName;
+    List<ReimbursementTicket> reimbursementTicketList;
+
+    public Employee(int employeeID, String username, String password, String position, String firstName, String lastName) {
+        this.employeeID = employeeID;
+        this.username = username;
+        this.password = password;
+        this.position = position;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<ReimbursementTicket> getReimbursementTicketList() {
+        return reimbursementTicketList;
+    }
+
+    public void setReimbursementTicketList(List<ReimbursementTicket> reimbursementTicketList) {
+        this.reimbursementTicketList = reimbursementTicketList;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeID=" + employeeID +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", position='" + position + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/revature/models/ReimbursementTicket.java
+++ src/main/java/com/revature/models/ReimbursementTicket.java
@@ -6,4 +6,63 @@ public class ReimbursementTicket {
         private String reimbursementStatus;
         private double reimbursementAmount;
         private String reimbursementCategory;
+
+        public ReimbursementTicket(int reimbursementID, int employeeID, String reimbursementStatus, double reimbursementAmount, String reimbursementCategory) {
+                this.reimbursementID = reimbursementID;
+                this.employeeID = employeeID;
+                this.reimbursementStatus = reimbursementStatus;
+                this.reimbursementAmount = reimbursementAmount;
+                this.reimbursementCategory = reimbursementCategory;
+        }
+
+        public int getReimbursementID() {
+                return reimbursementID;
+        }
+
+        public void setReimbursementID(int reimbursementID) {
+                this.reimbursementID = reimbursementID;
+        }
+
+        public int getEmployeeID() {
+                return employeeID;
+        }
+
+        public void setEmployeeID(int employeeID) {
+                this.employeeID = employeeID;
+        }
+
+        public String getReimbursementStatus() {
+                return reimbursementStatus;
+        }
+
+        public void setReimbursementStatus(String reimbursementStatus) {
+                this.reimbursementStatus = reimbursementStatus;
+        }
+
+        public double getReimbursementAmount() {
+                return reimbursementAmount;
+        }
+
+        public void setReimbursementAmount(double reimbursementAmount) {
+                this.reimbursementAmount = reimbursementAmount;
+        }
+
+        public String getReimbursementCategory() {
+                return reimbursementCategory;
+        }
+
+        public void setReimbursementCategory(String reimbursementCategory) {
+                this.reimbursementCategory = reimbursementCategory;
+        }
+
+        @Override
+        public String toString() {
+                return "ReimbursementTicket{" +
+                        "reimbursementID=" + reimbursementID +
+                        ", employeeID=" + employeeID +
+                        ", reimbursementStatus='" + reimbursementStatus + '\'' +
+                        ", reimbursementAmount=" + reimbursementAmount +
+                        ", reimbursementCategory='" + reimbursementCategory + '\'' +
+                        '}';
+        }
 }
