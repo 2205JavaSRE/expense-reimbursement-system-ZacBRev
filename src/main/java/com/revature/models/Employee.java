9cf094f5219a9267045fdce04021cc680a1699f1
ZacBurnett <zachary710@revature.net>
2022-05-31 07:26:01 -0700

    Initial Commit Creation of Project POJO - Employee and ReimbursementTicket

---
+++ src/main/java/com/revature/models/Employee.java
@@ -0,0 +1,5 @@
+package com.revature.models;
+
+public class Employee {
+    private int employeeID;
+}
---
+++ src/main/java/com/revature/models/ReimbursementTicket.java
@@ -0,0 +1,9 @@
+package com.revature.models;
+
+public class ReimbursementTicket {
+        private int reimbursementID;
+        private int employeeID;
+        private String reimbursementStatus;
+        private double reimbursementAmount;
+        private String reimbursementCategory;
+}
